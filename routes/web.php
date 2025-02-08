5d418a285e02c506202c33a662a369e652c069d9
Maxxksim <makximchagin@gmail.com>
2025-02-08 22:50:17 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UrlController;
+use App\Models\Url;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UrlController::class, 'index']);
+Route::post('/get-short-url', [UrlController::class, 'storeShortUrl'])->name('get-short-url');
+Route::get('/{shorted_url}', [UrlController::class, 'redirectToOriginalUrl']);
+
+
