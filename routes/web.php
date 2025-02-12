0f169e2f03d86f2197ca3b09c0c22f4d90a1bfe0
Maxxksim <makximchagin@gmail.com>
2025-02-12 21:13:49 +0200

    add route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Models\Url;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UrlController::class, 'index']);
-Route::post('/get-short-url', [UrlController::class, 'storeShortUrl'])->name('get-short-url');
 Route::get('/{shorted_url}', [UrlController::class, 'redirectToOriginalUrl']);
-
+Route::post('/url/create', [UrlController::class, 'createShortUrl']);
+Route::delete('/url/{url}', [UrlController::class, 'deleteShortUrl']);
 
